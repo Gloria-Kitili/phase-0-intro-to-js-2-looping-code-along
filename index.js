8724f27e3ac274688574599f3fe1efecf2a6785d
gloria kitili <gloria.kitili@student.moringaschool.com>
2022-11-20 18:08:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName){
+  var result = []
+  for (let counter= 0; counter < names.length; counter++) {
+    var ans = `Thank you, ${names[counter]}, for the wonderful ${eventName} gift!`;
+    result.push(ans);
+
+  }
+  console.log(result);
+  return result;
+}
+function countDown(num){
+  while(num >= 0){
+    console.log(num);
+    num--;
+  }
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
